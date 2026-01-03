a7939011ebdd1f7edee97798eab05b43e7fc62b7
WU-SUNFLOWER <2583727181@qq.com>
2026-01-04 02:26:27 +0800

    搭建开发环境

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # S.A.A.U.S.O
  
+```shell
+gn gen out/release
+ninja -C out/release --verbose
+gn gen out/release --export-compile-commands
+```
+
+```shell
+gn gen out/debug --args="is_debug=true"
+ninja -C out/debug --verbose
+gn gen out/debug --export-compile-commands
+```
+
+```shell
+gn gen out/asan --args="is_asan=true"
+ninja -C out/asan --verbose
+gn gen out/asan --export-compile-commands
+```
--- src/main.cc
+++ src/main.cc
@@ -1,25 +1,34 @@
 #include <iostream>
-#include "build/build_config.h" // 引入我们的探测头文件
 
-void PlatformSpecificFunction() {
-    // 这种写法优于 #ifdef，因为编译器会检查所有分支的语法
-    if (BUILDFLAG(IS_WIN)) {
-        std::cout << "[Logic] I am running on Windows!" << std::endl;
-    } 
-    
-    if (BUILDFLAG(IS_LINUX)) {
-        std::cout << "[Logic] I am running on Linux!" << std::endl;
-    }
+#include "build/build_config.h"  // 引入我们的探测头文件
+
+void PlatformSpecificFunction(int x, int y) {
+  // 这种写法优于 #ifdef，因为编译器会检查所有分支的语法
+  if (BUILDFLAG(IS_WIN)) {
+    std::cout << "[Logic] I am running on Windows!" << std::endl;
+  }
+
+  if (BUILDFLAG(IS_LINUX)) {
+    std::cout << "[Logic] I am running on Linux!" << std::endl;
+  }
+}
+
+void TestAsan() {
+  int* ar = new int[10];
+  ar[10086] = 123;
+  std::cout << "unreachable!!!" << std::endl;
 }
 
 int main() {
-    std::cout << "Hello, BuildFlags!" << std::endl;
-    
-    PlatformSpecificFunction();
+  std::cout << "Hello, BuildFlags!" << std::endl;
 
-    // 你甚至可以直接打印宏的值
-    std::cout << "Debug info: IS_WIN=" << BUILDFLAG(IS_WIN) 
-              << ", IS_LINUX=" << BUILDFLAG(IS_LINUX) << std::endl;
+  PlatformSpecificFunction(1, 2);
 
-    return 0;
+  // 你甚至可以直接打印宏的值
+  std::cout << "Debug info: IS_WIN=" << BUILDFLAG(IS_WIN)
+            << ", IS_LINUX=" << BUILDFLAG(IS_LINUX) << std::endl;
+
+  TestAsan();
+
+  return 0;
 }
