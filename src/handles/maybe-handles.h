f18b876614f0c5cf79bd63d38433b014fb373e18
WU-SUNFLOWER <47438910+WU-SUNFLOWER@users.noreply.github.com>
2026-02-21 01:49:00 +0800

    Update maybe-handles.h

--- src/handles/maybe-handles.h
+++ src/handles/maybe-handles.h
@@ -6,38 +6,84 @@
 #define SAAUSO_HANDLES_MAYBE_HANDLES_H_
 
 #include <cassert>
+#include <cstdio>
+#include <cstdlib>
 
 #include "src/handles/handles.h"
 
 namespace saauso::internal {
 
+struct NullMaybeHandleType {};
+
+inline constexpr NullMaybeHandleType kNullMaybeHandle;
+
 // 用于表示“可能成功也可能失败”的 Handle 返回值。
-// 该类型本身不携带错误信息，错误信息应由解释器的异常状态容器统一持有与传播。
+// 该类型本身不携带错误信息，错误信息由Isolate的异常状态容器统一持有与传播。
 template <typename T>
 class MaybeHandle {
  public:
   MaybeHandle() = default;
-  MaybeHandle(const MaybeHandle&) = default;
-  MaybeHandle& operator=(const MaybeHandle&) = default;
 
-  explicit MaybeHandle(Handle<T> handle) : handle_(handle) {}
+  explicit MaybeHandle(NullMaybeHandleType) : MaybeHandle() {}
 
-  bool is_null() const { return handle_.is_null(); }
+  // 支持将Handle向下转换为MaybeHandle
+  // 例如将Handle<PyList>转换成MaybeHandle<PyObject>
+  template <typename S>
+  constexpr MaybeHandle(Handle<S> handle)
+    requires(is_subtype_v<S, T>)
+      : MaybeHandle(handle.location()) {}
 
-  Handle<T> ToHandleChecked() const {
-    assert(!is_null());
-    return handle_;
+  // 支持MaybeHandle的向下转换
+  // 例如将Handle<PyList>转换成MaybeHandle<PyObject>
+  template <typename S>
+  constexpr MaybeHandle(MaybeHandle<S> maybe_handle)
+    requires(is_subtype_v<S, T>)
+      : MaybeHandle(maybe_handle.location()) {}
+
+  explicit MaybeHandle(Tagged<T> tagged) : MaybeHandle(handle(tagged)) {};
+
+  void Check() const {
+    if (is_null()) {
+      std::fprintf(stderr, "fatal null MaybeHandle");
+      std::exit(1);
+    }
   }
 
-  Handle<T> ToHandleNoThrow() const { return handle_; }
+  Handle<T> ToHandleChecked() const {
+    Check();
+    return Handle<T>(location_);
+  }
 
-  static MaybeHandle<T> Null() { return MaybeHandle<T>(); }
+  // 常规API: 将MaybeHandle转换为普通Handle。
+  // 调用该API时，要求T可以向下转换为S，否则编译阶段即会报错！
+  template <typename S>
+  bool ToHandle(Handle<S>* out) const {
+    if (is_null()) {
+      *out = Handle<T>::null();
+      return false;
+    } else {
+      *out = Handle<T>(location_);
+      return true;
+    }
+  }
+
+  constexpr bool is_null() const { return location_ == nullptr; }
+  constexpr static MaybeHandle<T> null() { return MaybeHandle<T>(); }
+
+  constexpr Address* location() const { return location_; }
+
+  // 快速检测两个MaybeHandle是否指向同一个对象
+  constexpr bool is_identical_to(const MaybeHandle<T> other) const {
+    return location() == other.location() || operator*() == *other;
+  }
 
  private:
-  Handle<T> handle_{Handle<T>::null()};
+  template <typename>
+  friend class MaybeHandle;
+
+  Address* location_{nullptr};
 };
 
 }  // namespace saauso::internal
 
 #endif  // SAAUSO_HANDLES_MAYBE_HANDLES_H_
-
