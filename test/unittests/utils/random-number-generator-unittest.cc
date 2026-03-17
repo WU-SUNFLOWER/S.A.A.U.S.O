ff81f320b483f27496930a39bb96f1e3628fc0f5
WU-SUNFLOWER <47438910+WU-SUNFLOWER@users.noreply.github.com>
2026-03-17 17:32:52 +0800

    Update random-number-generator-unittest.cc
    替换单元测试中随机数API的调用

--- test/unittests/utils/random-number-generator-unittest.cc
+++ test/unittests/utils/random-number-generator-unittest.cc
@@ -12,8 +12,8 @@ namespace saauso::internal {
 TEST(RandomNumberGeneratorTest, DeterministicForSameSeed) {
   RandomNumberGenerator rng1;
   RandomNumberGenerator rng2;
-  rng1.Seed(123456789);
-  rng2.Seed(123456789);
+  rng1.SetSeed(123456789);
+  rng2.SetSeed(123456789);
 
   for (int i = 0; i < 64; i++) {
     EXPECT_EQ(rng1.NextU64(), rng2.NextU64());
@@ -22,7 +22,7 @@ TEST(RandomNumberGeneratorTest, DeterministicForSameSeed) {
 
 TEST(RandomNumberGeneratorTest, NextDouble01Range) {
   RandomNumberGenerator rng;
-  rng.Seed(0);
+  rng.SetSeed(0);
 
   for (int i = 0; i < 10000; i++) {
     double x = rng.NextDouble01();
@@ -33,10 +33,10 @@ TEST(RandomNumberGeneratorTest, NextDouble01Range) {
 
 TEST(RandomNumberGeneratorTest, NextU64BoundedRange) {
   RandomNumberGenerator rng;
-  rng.Seed(42);
+  rng.SetSeed(42);
 
-  const uint64_t bounds[] = {1, 2, 3, 4, 5, 7, 8, 9, 10, 97, 1000,
-                             (1ULL << 32), (1ULL << 63)};
+  const uint64_t bounds[] = {
+      1, 2, 3, 4, 5, 7, 8, 9, 10, 97, 1000, (1ULL << 32), (1ULL << 63)};
   for (uint64_t bound : bounds) {
     for (int i = 0; i < 1000; i++) {
       uint64_t x = rng.NextU64Bounded(bound);
@@ -46,4 +46,3 @@ TEST(RandomNumberGeneratorTest, NextU64BoundedRange) {
 }
 
 }  // namespace saauso::internal
-
