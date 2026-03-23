cf258abdf0edda1898c93be7b1e21ec1ed74034a
WU-SUNFLOWER <47438910+WU-SUNFLOWER@users.noreply.github.com>
2026-03-23 21:10:44 +0800

    将scope移动到public isolate

--- include/saauso-isolate.h
+++ include/saauso-isolate.h
@@ -14,16 +14,35 @@ struct IsolateCreateParams {};
 
 class Isolate {
  public:
+  // Isolate::Scope 是一个 RAII 辅助类，用于在当前线程中进入/退出指定的
+  // Isolate。 在构造时调用 Enter()，析构时调用 Exit()。
+  class Scope {
+    explicit Scope(Isolate* isolate) : isolate_(isolate) { isolate_->Enter(); }
+
+    ~Scope() { isolate_->Exit(); }
+
+    // 我们不允许对 Isolate::Scope 进行拷贝
+    Scope(const Scope&) = delete;
+    Scope& operator=(const Scope&) = delete;
+
+   private:
+    Isolate* const isolate_;
+  };
+
   static Isolate* New(
       const IsolateCreateParams& params = IsolateCreateParams());
-  void Dispose();
-  void ThrowException(Local<Value> exception);
-  size_t ValueRegistrySizeForTesting() const;
 
   Isolate() = delete;
   ~Isolate() = delete;
   Isolate(const Isolate&) = delete;
   Isolate& operator=(const Isolate&) = delete;
+
+  void Enter();
+  void Exit();
+
+  void Dispose();
+  void ThrowException(Local<Value> exception);
+  size_t ValueRegistrySizeForTesting() const;
 
   void* operator new(size_t size) = delete;
   void* operator new[](size_t size) = delete;
