a37cd6d6569b2bd9eef3b7cc24b5c8f459dcff6c
WU-SUNFLOWER <47438910+WU-SUNFLOWER@users.noreply.github.com>
2026-03-28 01:33:25 +0800

    PyDict 容器原语去Isolate::Current()

--- src/objects/py-dict.h
+++ src/objects/py-dict.h
@@ -15,34 +15,43 @@ class PyDict : public PyObject {
  public:
   static constexpr int64_t kMinimumCapacity = 8;
 
-  static Handle<PyDict> NewInstance(int64_t init_capacity = kMinimumCapacity);
+  static Handle<PyDict> New(Isolate* isolate,int64_t init_capacity = kMinimumCapacity);
 
   static Tagged<PyDict> cast(Tagged<PyObject> object);
 
   int64_t capacity() const;
   Handle<PyObject> KeyAtIndex(int64_t index) const;
   Handle<PyObject> ValueAtIndex(int64_t index) const;
-  Handle<PyTuple> ItemAtIndex(int64_t index) const;
+  Handle<PyTuple> ItemAtIndex(int64_t index, Isolate* isolate) const;
 
-  Maybe<bool> ContainsKey(Handle<PyObject> key) const;
+  Maybe<bool> ContainsKey(Handle<PyObject> key, Isolate* isolate) const;
 
   // 根据 key 查 value 系列 API
   // - 返回 true : 成功查询到 value，通过 out 输出。
   // - 返回 false : 未查询到 value，out 输出 null。
   // - 返回空 Maybe : 查询过程中抛出异常，需要通知调用者向上传播异常，out 输出
   //   null。
-  Maybe<bool> Get(Handle<PyObject> key, Handle<PyObject>& out) const;
-  Maybe<bool> Get(Tagged<PyObject> key, Handle<PyObject>& out) const;
-  Maybe<bool> GetTagged(Handle<PyObject> key, Tagged<PyObject>& out) const;
-  Maybe<bool> GetTagged(Tagged<PyObject> key, Tagged<PyObject>& out) const;
+  Maybe<bool> Get(Handle<PyObject> key,
+                  Handle<PyObject>& out,
+                  Isolate* isolate) const;
+  Maybe<bool> Get(Tagged<PyObject> key,
+                  Handle<PyObject>& out,
+                  Isolate* isolate) const;
+  Maybe<bool> GetTagged(Handle<PyObject> key,
+                        Tagged<PyObject>& out,
+                        Isolate* isolate) const;
+  Maybe<bool> GetTagged(Tagged<PyObject> key,
+                        Tagged<PyObject>& out,
+                        Isolate* isolate) const;
 
-  Maybe<bool> Remove(Handle<PyObject> key);
+  Maybe<bool> Remove(Handle<PyObject> key, Isolate* isolate);
 
   int64_t occupied() const { return occupied_; }
 
   static Maybe<bool> Put(Handle<PyDict> dict,
                          Handle<PyObject> key,
-                         Handle<PyObject> value);
+                         Handle<PyObject> value,
+                         Isolate* isolate);
   static Handle<PyTuple> GetKeyTuple(Handle<PyDict> dict, Isolate* isolate);
 
   Handle<FixedArray> data() const;
@@ -51,8 +60,7 @@ class PyDict : public PyObject {
   friend class PyDictKlass;
   friend class Factory;
 
-  static void ExpandImpl(Handle<PyDict> list);
-  static Maybe<bool> ExpandImplMaybe(Handle<PyDict> dict);
+  static Maybe<bool> ExpandImpl(Handle<PyDict> dict, Isolate* isolate);
 
   // FixedArray* data_
   Tagged<PyObject> data_;
