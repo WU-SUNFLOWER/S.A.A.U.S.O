73b21c1eb49a1f4c2e36cc9b3f5734a2b9c9c0d8
WU-SUNFLOWER <47438910+WU-SUNFLOWER@users.noreply.github.com>
2026-04-11 22:09:00 +0800

    Update interpreter.cc
    剔除Interpreter::pending_exception

--- src/interpreter/interpreter.cc
+++ src/interpreter/interpreter.cc
@@ -57,10 +57,6 @@ Tagged<PyObject> Interpreter::pending_exception_tagged() const {
   return isolate_->exception_state()->pending_exception_tagged();
 }
 
-Handle<PyObject> Interpreter::pending_exception() const {
-  return isolate_->exception_state()->pending_exception(isolate_);
-}
-
 void Interpreter::ClearPendingException() {
   isolate_->exception_state()->Clear();
 }
