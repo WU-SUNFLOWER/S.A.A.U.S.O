78118361400dad183fe86558eda146d36c02a01f
WU-SUNFLOWER <47438910+WU-SUNFLOWER@users.noreply.github.com>
2026-04-12 19:48:30 +0800

    移除具体单测对 interpreter 这一虚拟机内部执行细节的依赖

--- test/unittests/interpreter/interpreter-closure-unittest.cc
+++ test/unittests/interpreter/interpreter-closure-unittest.cc
@@ -6,7 +6,6 @@
 #include "src/execution/isolate.h"
 #include "src/heap/factory.h"
 #include "src/handles/handles.h"
-#include "src/interpreter/interpreter.h"
 #include "src/objects/py-float.h"
 #include "src/objects/py-list.h"
 #include "src/objects/py-smi.h"
